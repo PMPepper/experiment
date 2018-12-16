2f20a993e36b438e8c7c2fa892b0c335a1aa9635
Paul Pepper <pm_pepper@hotmail.com>
2018-12-16 11:34:49 +0000

    Implemented CSS Modules + build minification + autoprefixer

--- src/js/components/test/Test.js
+++ src/js/components/test/Test.js
@@ -5,11 +5,6 @@ import styles from './styles.css';
 export default function Test() {
   return <div className={styles.test}>
     <h2 className={styles.title}>Test!</h2>
-    <p className={styles.p}>This is some text right here!?</p>
+    <p className={styles.p}>This is some text right here.</p>
   </div>
-}
-
-
-if(process.env.NODE_ENV !== 'production') {
-  module.hot.accept();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 const config = {
   entry: './src/js/main.js',
@@ -13,20 +15,6 @@ const config = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader" },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[local]_[hash:base64:6]',
-            }
-          }
-        ]
       },
     ]
   },
@@ -49,16 +37,55 @@ const config = {
   }
 };
 
+const cssLoaderConfig = {
+  loader: "css-loader",
+  options: {
+    sourceMap: true,
+    modules: true,
+    localIdentName: '[local]_[hash:base64:6]',
+  }
+};
+
 module.exports = (env, argv) => {
   const mode = argv.mode || 'development';
   config.mode = mode;
 
   if (mode === 'development') {
+    /////////////////
+    // Development //
+    /////////////////
     config.devtool = 'source-map';
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        { loader: "style-loader" },
+        //MiniCssExtractPlugin.loader,
+        cssLoaderConfig
+      ]
+    });
 
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
   } else if(mode === 'production') {
+    ////////////////
+    // PRODUCTION //
+    ////////////////
     config.devtool = 'hidden-source-map';
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        //{ loader: "style-loader" },
+        MiniCssExtractPlugin.loader,
+        cssLoaderConfig,
+        'postcss-loader'
+      ]
+    });
+
+    config.plugins.push(new MiniCssExtractPlugin({
+      filename: '../css/style.css',
+    }));
+
     config.optimization.minimizer = [new UglifyJsPlugin({
       uglifyOptions: {
         output: {
