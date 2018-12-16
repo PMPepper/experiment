4b7125beb5ae09804543b5392e8e54c18f764927
Paul Pepper <pm_pepper@hotmail.com>
2018-12-16 09:51:57 +0000

    Initial css-modules support added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-alert('Hello world!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Test from './components/test/Test';
+
+const title = 'Testing CSS modules';
+
+ReactDOM.render(
+  <div>
+    <h1>{title}</h1>
+    <p>Hello world blah blah blah!</p>
+    <Test />
+  </div>,
+  document.getElementById('app')
+);
+
+
+if(process.env.NODE_ENV !== 'production') {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,77 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   entry: './src/js/main.js',
+  optimization: {
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[local]_[hash:base64:6]',
+            }
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build/js'),
+    publicPath: "/js/",
     filename: 'scripts.js'
+  },
+  plugins: [
+
+  ],
+  devServer: {
+    stats: "errors-only",
+    contentBase: './dist',
+    overlay: true,
+    hot: true,
   }
 };
 
 module.exports = (env, argv) => {
   const mode = argv.mode || 'development';
   config.mode = mode;
-  
+
   if (mode === 'development') {
     config.devtool = 'source-map';
+
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
   } else if(mode === 'production') {
-	
+    config.devtool = 'hidden-source-map';
+    config.optimization.minimizer = [new UglifyJsPlugin({
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+      },
+      extractComments: true
+    })]
   } else {
-	throw new Error('Invalid mode: '+argv.mode);
+	   throw new Error('Invalid mode: '+argv.mode);
   }
+
+  console.log(config);
 
   return config;
 };
